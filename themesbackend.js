356e7e1ccdbd8c0f99797d378a4076647587819c
RaptorCoding <146634263+RaptorCoding@users.noreply.github.com>
2026-03-22 04:12:50 +0100

    Create themesbackend.js

---
+++ themesbackend.js
@@ -0,0 +1,15 @@
+{
+  "themes": [
+    {
+      "name": "Cattpuccin Machiatto - Lavender",
+      "author": "raptor#0151",
+      "preview": "https://www.example.com/preview.png",
+      "download": "https://www.example.com/theme.css"
+    },
+    }
+      "name": "Remove rounding",
+      "author": "raptor#0151"
+      "preview": "https://www.example.com/preview.png",
+      "download": "https://www.example.com/theme.css"
+  ]
+}
