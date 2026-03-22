6b3cb3a5b921d04a733c3b62e26d9deffc9e021b
RaptorCoding <146634263+RaptorCoding@users.noreply.github.com>
2026-03-22 04:31:29 +0100

    Update themesbackend.js

--- themesbackend.js
+++ themesbackend.js
@@ -6,7 +6,7 @@
       "preview": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/preview.png",
       "download": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/Cattpuccin_Macchiato_Lavender.css"
     },
-    }
+    {
       "name": "Remove rounding",
       "author": "raptor#0151",
       "preview": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/preview.png",
