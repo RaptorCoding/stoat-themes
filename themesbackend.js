acab1fe61c824a8e73b3c4c4ca7e04872183307a
RaptorCoding <146634263+RaptorCoding@users.noreply.github.com>
2026-08-01 03:19:05 +0200

    Update themesbackend.js

--- themesbackend.js
+++ themesbackend.js
@@ -1,10 +1,34 @@
 {
   "themes": [
     {
-      "name": "Cattpuccin",
+      "name": "Cattpuccin Macchiato",
       "author": "raptor#0151",
       "preview": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/preview.png",
-      "download": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/Cattpuccin_themes_for_avia.zip"
+      "download": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/Cattpuccin_Macchiato.css"
+    },
+    {
+  "themes": [
+    {
+      "name": "Cattpuccin Latte",
+      "author": "raptor#0151",
+      "preview": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/preview.png",
+      "download": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/Cattpuccin_Latte.css"
+    },
+    {
+  "themes": [
+    {
+      "name": "Cattpuccin Mocha",
+      "author": "raptor#0151",
+      "preview": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/preview.png",
+      "download": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/Cattpuccin_Mocha.css"
+    },
+    {
+  "themes": [
+    {
+      "name": "Cattpuccin Frappé",
+      "author": "raptor#0151",
+      "preview": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/preview.png",
+      "download": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/Cattpuccin_Frappé.css"
     },
     {
       "name": "Remove rounding",
