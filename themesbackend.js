25649bfbadd429a4a03514f1a97ad908d9a561a0
RaptorCoding <146634263+RaptorCoding@users.noreply.github.com>
2026-08-01 03:22:33 +0200

    Update themesbackend.js

--- themesbackend.js
+++ themesbackend.js
@@ -33,7 +33,7 @@
     {
       "name": "Remove rounding",
       "author": "raptor#0151",
-      "preview": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/preview.png",
+      "preview": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/Preview_r.png",
       "download": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/square_everything.css"
     }
   ]
