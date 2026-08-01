2fc4244afaa8bd870e5a31018687b1c35d2eb65f
RaptorCoding <146634263+RaptorCoding@users.noreply.github.com>
2026-08-01 03:29:17 +0200

    Update themesbackend.js

--- themesbackend.js
+++ themesbackend.js
@@ -6,24 +6,20 @@
       "preview": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/preview.png",
       "download": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/Cattpuccin_Macchiato.css"
     },
-    {
-  "themes": [
+
     {
       "name": "Cattpuccin Latte",
       "author": "raptor#0151",
       "preview": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/preview.png",
       "download": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/Cattpuccin_Latte.css"
     },
-    {
-  "themes": [
+
     {
       "name": "Cattpuccin Mocha",
       "author": "raptor#0151",
       "preview": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/preview.png",
       "download": "https://raw.githubusercontent.com/RaptorCoding/stoat-themes/refs/heads/main/Cattpuccin_Mocha.css"
     },
-    {
-  "themes": [
     {
       "name": "Cattpuccin Frappé",
       "author": "raptor#0151",
